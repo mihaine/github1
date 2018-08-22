7a82d4c0f2e206411d555f705821f8064864fb77
Mihai Negrean <mihai@2603-9001-1489-7a00-0c72-8e42-2bc5-d767.res.spectrum.com>
2018-08-22 18:30:28 -0400

    Initial Commit

---
+++ MP1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MP1
+//
+//  Created by Mihai Negrean on 8/22/18.
+//  Copyright © 2018 Mihai Negrean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
